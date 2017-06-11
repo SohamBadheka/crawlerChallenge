d64a588cf752476624dd61b84ad21091f3397919
Soham Badheka <sohambadheka008@gmail.com>
2017-06-10 19:03:08 -0700

    Create App.java

--- src/main/java/com/parser/App.java
+++ src/main/java/com/parser/App.java
@@ -22,7 +22,6 @@ public class App
         System.out.println( "Parsing the web page..."+args[0] );
         App app = new App();
         
-        
         app.startApp(args);
 
     }
@@ -30,24 +29,21 @@ public class App
     	//System.out.println("args is "+args[0]);
     	String url = "";
     	if(args.length<=0) throw new Exception("Please provide the URL to parse the page");
-    	else{
-    		url = args[0];
-    	}
+    	else url = args[0];
+    	
     	//"http://www.amazon.com/Cuisinart-CPT-122-Compact-2-Slice-Toaster/dp/B009GQ034C/ref=sr_1_1?s=kitchen&ie=UTF8&qid=1431620315&sr=1-1&keywords=toaster";
     	//String url = "http://blog.rei.com/camp/how-to-introduce-your-indoorsy-friend-to-the-outdoors/";
     	urlParser = new UrlParser(url);
     	
     	//Check if the URL is correct, else throw an exception
         Document doc = urlParser.validateUrl(url);
-	    Elements header = doc.select("h1");
-	    String paraContent = doc.select("p").first().text();
-	    String headerContent = header.text();
+	Elements header = doc.select("h1");
+	String paraContent = doc.select("p").first().text();
+	String headerContent = header.text();
         String mainContent = doc.body().text();
         System.out.println(headerContent+ " "+ paraContent);
         //Parse the document content to get the important keywords
-	    docParser = new DocParser(mainContent, headerContent, paraContent);
-	   
-	 
+	docParser = new DocParser(mainContent, headerContent, paraContent);
 	    
     }
 }
